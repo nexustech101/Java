99e0da35fc5b027fb4b0f3ae8ab907e5b2ed843d
Charles Defreese III <charlesldefreeseiii@gmail.com>
2024-05-04 05:57:43 -0400

    Initial commit for LinkedList Tutorial

--- script.bat
+++ script.bat
@@ -1,17 +1,6 @@
-# Initialize the git repository
+
 git init .
-
-# Write some code
-touch README.md
-
-# Add the files to the staging area
 git add .
-
-# Commit the changes to the local repository
 git commit -m "Initial commit for LinkedList Tutorial"
-
-# List the remote repositories
 git remote -v
-
-# Push the changes to the remote repository
 git push origin master
