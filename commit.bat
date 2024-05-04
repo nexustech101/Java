73c78b756bc9a73534a5244bab2915ec073bb788
Charles Defreese III <charlesldefreeseiii@gmail.com>
2024-05-04 07:05:30 -0400

    Resolved merge conflicts and applied stashed changes

--- commit.bat
+++ commit.bat
@@ -8,19 +8,16 @@ rem Add all files to the staging area
 git add .
 
 rem Commit changes with a default message
-git commit -m "Initial commit"
+@REM git commit -m "Initial commit"
 
 rem Set the remote origin to the GitHub repository URL including the subdirectory path
-git remote add origin https://github.com/nexustech101/Java/tree/main/Algorithms%20%26%20Data%20Structures
+git remote add origin https://github.com/nexustech101/Java.git
 
 rem Pull changes from the main branch of the remote repository
-git pull origin main --allow-unrelated-histories
+git pull origin main --allow-unrelated-histories 
 
 rem Push changes to the main branch of the remote repository
 git push -u origin main
-
-rem Ignore this script file by adding it to the .gitignore file
-echo commit.bat >> .gitignore
 
 rem Display success message
 echo Initial commit pushed to GitHub successfully!
