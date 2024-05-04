6916eb29ef1654dbf9e2af995838b5c253edb000
Charles Defreese III <charlesldefreeseiii@gmail.com>
2024-05-04 06:02:30 -0400

    Initial commit for LinkedList Tutorial

--- script.bat
+++ script.bat
@@ -3,4 +3,4 @@ git init .
 git add .
 git commit -m "Initial commit for LinkedList Tutorial"
 git remote add origin https://github.com/nexustech101/Java
-git push -u origin master
+git push -u origin main
